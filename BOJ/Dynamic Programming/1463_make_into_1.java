40dfe031eabe5a4027e17c22893a1cfda1347462
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-03-23 21:17:29 +0900

    1463 1로 만들기

--- BOJ/Dynamic Programming/1463_make_into_1.java
+++ BOJ/Dynamic Programming/1463_make_into_1.java
@@ -11,10 +11,10 @@ public class MakeInto2 {
 		
 		for(int i=2; i<=n; i++) {
 			dp[i] = dp[i-1] + 1;
-      
+			
 			if(i%2 == 0)
 				dp[i] = Math.min(dp[i], dp[i/2] + 1);
-      
+			
 			if(i%3 == 0)
 				dp[i] = Math.min(dp[i], dp[i/3] + 1);
 		}
@@ -32,10 +32,10 @@ public class MakeInto2 {
 			
 			dp[i-1] = Math.min(dp[i-1], dp[i] + 1);
 			
-      if(i%2 == 0)
+      			if(i%2 == 0)
 				dp[i/2] = Math.min(dp[i/2], dp[i] + 1);
 			
-      if(i%3 == 0)
+      			if(i%3 == 0)
 				dp[i/3] = Math.min(dp[i/3], dp[i] + 1);
 		}
     
