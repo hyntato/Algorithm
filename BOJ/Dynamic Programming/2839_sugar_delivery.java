85b71c333d4b55a5a094f17b8cd35a8a62073b9b
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-03-23 20:57:20 +0900

    2839 설탕 배달

---
+++ BOJ/Dynamic Programming/2839_sugar_delivery.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SugarDelivery {
+	
+	private static int findMinNumOfBags(int n) {
+		int count = 0;
+		
+		while(n >= 0) {
+			if(n%5 == 0)
+				return n/5 + count;
+			else
+        n -= 3;
+			count++;
+		}
+    
+		return -1;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(findMinNumOfBags(n));
+	}
+
+}
+
