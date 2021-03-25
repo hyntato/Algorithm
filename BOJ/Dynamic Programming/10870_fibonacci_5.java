50ff146e975635189e94300cd7718f296e1937f1
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-03-25 17:47:20 +0900

    10870 피보나치 수 5

---
+++ BOJ/Dynamic Programming/10870_fibonacci_5.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Fibonacci5 {
+	
+	private static int[] dp = new int[21];
+	
+	private static int fibo(int n) {
+		if(n == 0)
+			return 0;
+		if(n == 1)
+			return 1;
+		return fibo(n-1) + fibo(n-2);
+	}
+	
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		// 재귀
+		System.out.println(fibo(n));
+		
+		/*
+    // 반복문
+		for(int i=2; i<=n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+    System.out.println(dp[n]);
+    */
+	}
+
+}
