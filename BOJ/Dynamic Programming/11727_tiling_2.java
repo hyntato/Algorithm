8b534b89ad8cc6817a31bf3724e08c5a76e38292
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-03-28 16:50:35 +0900

    11727 2xn 타일링 2

---
+++ BOJ/Dynamic Programming/11727_tiling_2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tiling2 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n == 1)
+			System.out.println(1);
+		else if(n == 2)
+			System.out.println(3);
+		else {
+			int[] dp = new int[n+1];
+			dp[1] = 1;
+			dp[2] = 3;
+			
+			for(int i=3; i<=n; i++) {
+				dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+			}
+			System.out.println(dp[n]);
+		}
+	}
+
+}
