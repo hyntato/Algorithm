815595a5440a11f6cbf40fd0cbcc6b4095d6df68
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-04-15 19:48:04 +0900

    Level2 스킬트리

---
+++ Programmers/SkillTrees.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public int solution(String skill, String[] skill_trees) {
+        int count = 0;
+      
+        for (String skill_: skill_trees) {
+            skill_ = skill_.replaceAll("[^" + skill + "]", "");
+            if (skill.indexOf(skill_) == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
