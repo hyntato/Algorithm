2713d020741b4a4ca01acd051e9f970282ae004b
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-04-19 22:30:19 +0900

    Level2 가장 큰 수

---
+++ Programmers/LargestNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    
+    public String solution(int[] numbers) {
+        String[] nums = new String[numbers.length];
+      
+        for(int i=0; i<nums.length; i++) {
+            nums[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(nums, new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                return (str2+str1).compareTo(str1+str2);
+            }
+        });
+        
+        if(nums[0].equals("0"))
+            return "0";
+        
+        String answer = "";
+        for(int i=0; i<nums.length; i++) {
+            answer += nums[i];
+        }
+        return answer;
+    }
+    
+}
