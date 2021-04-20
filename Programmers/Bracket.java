78c9d0bbea5302749b59cbfcd9934c8e6494e9e1
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-04-21 00:26:21 +0900

    Level2 올바른 괄호

---
+++ Programmers/Bracket.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Bracket {
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+      
+        for(int i=0; i<s.length(); i++) {
+            char curr = s.charAt(i);
+            if(curr == '(')
+                stack.push(curr);
+            else if(!stack.isEmpty() && stack.peek() == '(')
+                stack.pop();
+            else {
+                stack.push(curr);
+                break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
