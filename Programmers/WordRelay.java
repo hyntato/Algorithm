507712c6f514087f0098000b71de63aa0bccbb2b
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-04-23 00:01:37 +0900

    Level2 영어 끝말잇기

---
+++ Programmers/WordRelay.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class WordRelay {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+        List<String> list = new ArrayList<String>();
+        list.add(words[0]);
+        
+        for(int i=1; i<words.length; i++) {
+            if(list.contains(words[i]) || words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
+                answer[0] = i%n + 1;
+                answer[1] = i/n + 1;
+                break;
+            }
+            list.add(words[i]);
+        }
+        return answer;
+    }
+}
