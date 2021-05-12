85fb04a8a95735ebc6579b0a0552c8652388a6db
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-05-12 14:22:40 +0900

    Level2 최댓값과 최솟값

---
+++ Programmers/MaxMin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] sArr = s.split(" ");
+        int[] nArr = new int[sArr.length];
+        
+        for(int i=0; i<nArr.length; i++) {
+            nArr[i] = Integer.parseInt(sArr[i]);
+        }
+        Arrays.sort(nArr);
+        
+        return nArr[0] + " " + nArr[nArr.length-1];
+    }
+}
