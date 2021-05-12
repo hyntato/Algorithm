3077a9ac01e0804a876f437ce70053c57fe070ac
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-05-12 14:31:49 +0900

    Level2 행렬의 곱셈

---
+++ Programmers/Level2/MatrixMulti.java
@@ -0,0 +1,17 @@
+class MatrixMulti {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int r1 = arr1.length;
+        int c1, r2 = arr1[0].length;
+        int c2 = arr2[0].length;
+        
+        int[][] answer = new int[r1][c2];
+        for(int i=0; i<r1; i++) {
+            for(int j=0; j<c2; j++) {
+                for(int k=0; k<r2; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
