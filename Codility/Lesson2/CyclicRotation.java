cc63912888322a29f3e014b4df3ae4fbcb9efcbf
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-06-17 22:12:44 +0900

    Lesson2 CyclicRotation

---
+++ Codility/Lesson2/CyclicRotation.java
@@ -0,0 +1,9 @@
+class CyclicRotation {
+    public int[] solution(int[] A, int K) {
+        int[] result = new int[A.length];
+        for(int i=0; i<A.length; i++)  {
+            result[(i+K)%A.length] = A[i];
+        }
+        return result;
+    }
+}
