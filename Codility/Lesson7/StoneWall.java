6475e98397b6de2c89fb4a630f4e075dbe4089ec
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-06-19 17:13:05 +0900

    Lesson7 StoneWall

---
+++ Codility/Lesson7/StoneWall.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] H) {
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for(int h: H) {
+            while(true) {
+                if(stack.isEmpty() || stack.peek() < h) {
+                    stack.push(h);
+                    break;
+                }
+                if(stack.peek() == h) {
+                    break;
+                }
+                if(stack.peek() > h) {
+                    stack.pop();
+                    count++;
+                }
+            } 
+        }
+
+        return count+stack.size();
+    }
+}
