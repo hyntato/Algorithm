ef1ce3bb3a13fe84054a8ece1a59baccfef61779
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-06-24 19:48:34 +0900

    14502 연구소

--- BOJ/Graph/14502_laboratory.java
+++ BOJ/Graph/14502_laboratory.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,56 +8,55 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Laboratory {
+	private static int maxSafeArea = 0;
 	
-	private static int max = 0;
-	
-	private static int n, m;
+	private static int N, M;
 	private static int[][] map;
-	private static List<Point> virusList = new ArrayList<Point>();
-	private static List<Point> blankList = new ArrayList<Point>();
 	
-	// combination: backtracking
-	private static void dfs(int start, int wallNum) {
-		if(wallNum == 0) {
-			max = Math.max(max, blankList.size() - bfs() - 3);
+	private static List<int[]> blankList = new ArrayList<>();
+	private static List<int[]> virusList = new ArrayList<>();
+
+	private static void combination(int start, int r) {
+		if(r == 0) {
+			int count = blankList.size()-bfs()-3;
+			if(maxSafeArea < count) {
+				maxSafeArea = count;
+			}
 			return;
 		}
-		
 		for(int i=start; i<blankList.size(); i++) {
-			Point p = blankList.get(i);
-			
-			map[p.x][p.y] = 1;
-			dfs(i+1, wallNum-1);
-			map[p.x][p.y] = 0;
+			int[] loc = blankList.get(i);
+			map[loc[0]][loc[1]] = 1;
+			combination(i+1, r-1);
+			map[loc[0]][loc[1]] = 0;
 		}
 	}
-	
+
 	private static int bfs() {
 		int count = 0;
-		
-		boolean[][] visited = new boolean[n][m];
-		int[] dx = {1, -1, 0, 0};
+		int[] dx = {-1, 1, 0, 0};
 		int[] dy = {0, 0, -1, 1};
-
-		Queue<Point> q = new LinkedList<Point>();
 		
-		for(int i=0; i<virusList.size(); i++) {
-			Point p = virusList.get(i);
-			q.offer(p);
-			visited[p.x][p.y] = true;
+		Queue<int[]> q = new LinkedList<>();
+		boolean[][] visited = new boolean[N][M];
+		
+		for(int[] loc: virusList) {
+			q.offer(loc);
+			visited[loc[0]][loc[1]] = true;
 		}
 		
 		while(!q.isEmpty()) {
-			Point p = q.poll();
+			int[] curr = q.poll();
+			int x = curr[0];
+			int y = curr[1];
 			
-			for(int i=0; i<4; i++) {
-				int nx = p.x + dx[i];
-				int ny = p.y + dy[i];
+			for(int i=0; i<dx.length; i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
 				
-				if(0<=nx && nx<n && 0<=ny && ny<m) {
-					if(!visited[nx][ny] && map[nx][ny]==0) {
-						
-						q.offer(new Point(nx, ny));
+				if(0<=nx && nx<N && 0<=ny && ny<M) {
+					if(!visited[nx][ny] && map[nx][ny] == 0) {
+						q.offer(new int[]{nx, ny});
 						visited[nx][ny] = true;
 						count++;
 					}
@@ -68,33 +66,31 @@ public class Laboratory {
 		return count;
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	
+		map = new int[N][M];
 		
-		map = new int[n][m];
-		
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(bf.readLine());
-			
-			for(int j=0; j<m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				
-				if(map[i][j] == 0) {
-					blankList.add(new Point(i, j));
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M; j++) {
+				int token = Integer.parseInt(st.nextToken());
+				if(token == 0) {
+					blankList.add(new int[]{i, j});
 				}
-				if(map[i][j] == 2) {
-					virusList.add(new Point(i, j));
+				if(token == 2) {
+					virusList.add(new int[]{i, j});
 				}
+				map[i][j] = token;
 			}
 		}
 		
-		dfs(0, 3);
-		System.out.println(max);
+		combination(0, 3);
+		System.out.println(maxSafeArea);
 	}
 
 }
