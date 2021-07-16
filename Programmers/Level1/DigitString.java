9c0300680f34595701d5cbf4e2f83f059379de58
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-07-16 20:58:17 +0900

    Level1 숫자 문자열과 영단어

---
+++ Programmers/Level1/DigitString.java
@@ -0,0 +1,9 @@
+class DigitString {
+    public int solution(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0; i<arr.length; i++) {
+            s = s.replace(arr[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
